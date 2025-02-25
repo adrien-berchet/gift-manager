6db3742436edd50ad0d3d290d6af906ad665c310
Adrien Berchet <adrien.berchet@gmail.com>
2025-02-26 00:22:41 +0100

    Chore: Refactor dropdown filter scripts

---
+++ gift_manager/static/gift_manager/datatables-filters.js
@@ -0,0 +1,48 @@
+(function($) {
+    $.fn.initDataTableFilters = function(options) {
+        var settings = $.extend({
+            order: [[0, 'asc']],
+            paging: true,
+            searching: true,
+            filterSelector: '.filter-icon',             // Filter icon to click on
+            dropdownSelector: '.dropdown-filter',       // Dropdown to show/hide
+            checkboxSelector: '.column-filter',         // Checkbox to filter on
+            thFilterClass: '',                          // <th> class to filter on
+        }, options);
+
+        var table = this.DataTable({
+            "order": settings.order,
+            "paging": settings.paging,
+            "searching": settings.searching
+        });
+
+        function applyColumnFilter() {
+            var selected = [];
+            $(settings.checkboxSelector + ':checked').each(function(){
+                selected.push($(this).val());
+            });
+            var regex = selected.length ? selected.join("|") : "";
+            var colIndex = $('th.' + settings.thFilterClass).index();
+            table.column(colIndex).search(regex, true, false).draw();
+        }
+
+        // Dropdown filter display
+        $(settings.filterSelector).on('click', function(e) {
+            e.stopPropagation();
+            $(this).siblings(settings.dropdownSelector).toggle();
+        });
+
+        // Hide dropdown when clicking outside
+        $(document).on('click', function() {
+            $(settings.dropdownSelector).hide();
+        });
+
+        // When a checkbox changes, apply filter
+        $(settings.checkboxSelector).on('change', function() {
+            applyColumnFilter();
+        });
+
+        // At load time, apply the filter if some checkboxes are already checked
+        applyColumnFilter();
+    };
+})(jQuery);
